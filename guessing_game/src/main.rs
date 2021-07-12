636b6c7453c575f69ba6aec3c77e4803ca7022d3
kamneklogs <kamneklogs@gmail.com>
2021-07-12 11:17:12 -0500

    Guessing game finished

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,25 +5,36 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("{}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1..101); //Limite inferior inclusivo, superior exclusivo
 
-    println!("Please input your guess");
+    //println!("The secret number is {}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess");
 
-    io::stdin()
-        .read_line(&mut guess) // .read_line() retorna un tipo Result, puede ser Ok or Err, commo Exception types
-        .expect("Failed to read line"); //Rompe el programa si el type Result es Err
+        let mut guess = String::new(); // mut permite que guess pueda cambiar de valor
 
-    println!("You guessed: {}", guess);
+        io::stdin() //Parecido a getLine() en c++
+            .read_line(&mut guess) // .read_line() retorna un tipo Result, puede ser Ok or Err, commo Exception types
+            .expect("Failed to read line"); //Rompe el programa si el type Result es Err
 
-    let guess: u32 = guess.trim().parse().expect("Error");
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        // Match -> Semejante a una sentencia switch
-        Ordering::Less => println!("Too small!"), //Ordering es un enum, puede tener esas tres variantes
-        Ordering::Greater => println!("Too big!"), //Cada opcion es llamada un arms o brazo, tiene un patron y el codigo a ejecutar si el match retorna dicho patron o variante
-        Ordering::Equal => println!("You win!"),
+        //: u32 entero sin signo de 32 bits, se expecifica para que parse() funcione correctamente
+        let guess: u32 = match guess.trim().parse() {
+            //Usamos match para capturar alguna excepcion
+            Ok(num) => num,
+            Err(_) => continue, //Si el parseo falla, se omite la ejecucion del resto del loop y se inicia la siguiente iteracion
+        };
+        match guess.cmp(&secret_number) {
+            // Match -> Semejante a una sentencia switch
+            Ordering::Less => println!("Too small!"), //Ordering es un enum, puede tener esas tres variantes
+            Ordering::Greater => println!("Too big!"), //Cada opcion es llamada un arms o brazo, tiene un patron y el codigo a ejecutar si el match retorna dicho patron o variante
+            Ordering::Equal => {
+                //Atencion a las variaciones de las arrow functions
+                println!("You win!");
+                break; //Curiosa forma de romper el loop
+            }
+        }
     }
 }
